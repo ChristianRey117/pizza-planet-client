f5842c3c8d7acf0510310c1870b42cb6cb0da99d
Christian <christianreyeshdz117@gmail.com>
2023-10-06 00:33:30 -0600

    feat(inventario): create catalogo inventario

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -1,32 +1,36 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import React from 'react';
-import Home from '../pages/Home';
-import AllFoods from '../pages/AllFoods';
-import FoodDetails from '../pages/FoodDetails';
-import Cart from '../pages/Cart';
-import Checkout from '../pages/Checkout';
-import Contact from '../pages/Contact';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
-import Sucursales from '../pages/Sucursales';
-import AdminMenu from '../pages/AdminMenu';
-import SucursalDashboard from '../pages/SucursalDashboard';
+import { Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
+import Home from "../pages/Home";
+import AllFoods from "../pages/AllFoods";
+import FoodDetails from "../pages/FoodDetails";
+import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Sucursales from "../pages/Sucursales";
+import AdminMenu from "../pages/AdminMenu";
+import SucursalDashboard from "../pages/SucursalDashboard";
+import InventarioDashboard from "../pages/Inventario/InventarioDashboard";
+import InventarioForm from "../pages/Inventario/InventarioForm";
 
 const Routers = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigate to='/home' />} />
-      <Route path='/home' element={<Home />} />
-      <Route path='/foods' element={<AllFoods />} />
-      <Route path='/foods/:id' element={<FoodDetails />} />
-      <Route path='/checkout' element={<Checkout />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-      <Route path='/contact' element={<Contact />} />
-      <Route path='/cart' element={<Cart />} />
-      <Route path='/sucursales' element={<Sucursales />} />
-      <Route path='/admin-menu' element={<AdminMenu />} />
-      <Route path='/sucursales-dashboard' element={<SucursalDashboard />} />
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/foods" element={<AllFoods />} />
+      <Route path="/foods/:id" element={<FoodDetails />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/sucursales" element={<Sucursales />} />
+      <Route path="/admin-menu" element={<AdminMenu />} />
+      <Route path="/sucursales-dashboard" element={<SucursalDashboard />} />
+      <Route path="/inventario-dashboard" element={<InventarioDashboard />} />
+      <Route path="/inventario-form" element={<InventarioForm />} />
     </Routes>
   );
 };
