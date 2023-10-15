245c2417cf283e1e0d765c1f88bbffc856352f6c
Christian <christianreyeshdz117@gmail.com>
2023-10-15 13:52:40 -0600

    feat(modal): create modal for alert to user

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -1,7 +1,19 @@
-import React from "react";
-
-const Modal = () => {
-  return <div></div>;
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+const ModalComponent = ({ show, handleClose, optionsModal }) => {
+  return (
+    <div>
+      <Modal isOpen={show} toggle={handleClose} backdrop="static">
+        <ModalHeader toggle={handleClose}>{optionsModal?.title}</ModalHeader>
+        <ModalBody>{optionsModal?.message}</ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={optionsModal.redirectTo}>
+            Continuar
+          </Button>{" "}
+        </ModalFooter>
+      </Modal>
+    </div>
+  );
 };
 
-export default Modal;
+export default ModalComponent;
