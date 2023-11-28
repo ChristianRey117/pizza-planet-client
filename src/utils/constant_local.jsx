0a034e90b99e386fdd43743820f747b5722df874
ALBERTO YAEL REYES HERNANDEZ <20090709@alumno.utmetropolitana.edu.mx>
2023-11-27 22:45:43 -0600

    endpoint local

---
+++ src/utils/constant_local.jsx
@@ -0,0 +1,43 @@
+const ENDPOINTS = {
+  BASE: "http://localhost:5000",
+  BASE_IMAGES:
+    "https://cs710032000dda411cc.blob.core.windows.net/pizza-planeta",
+  // LOGIN RUTAS
+  LOGIN: "http://localhost:5000/login",
+  // SUCURSALES RUTAS
+  SUCURSALES: "http://localhost:5000/sucursales",
+  SUCURSALES_ADD: "http://localhost:5000/sucursales/add",
+  // PROVEEDORES RUTAS
+  PROVEEDORES: "http://localhost:5000/proveedores",
+  PROVEEDORES_ADD: "http://localhost:5000/proveedores/add",
+  // PRODUCTOS RUTAS
+  PRODUCTOS: "http://localhost:5000/productos",
+  PRODUCTOS_ADD: "http://localhost:5000/productos/add",
+  // INVENTARIO RUTAS
+  INVENTARIO: "http://localhost:5000/inventario",
+  INVENTARIO_ADD: "http://localhost:5000/inventario/add",
+  // VECINDARIOS RUTAS
+  VECINDARIOS: "http://localhost:5000/vecindarios",
+  VECINDARIOS_ADD: "http://localhost:5000/vecindarios/add",
+  // TIPOSCATEGORIAS RUTAS
+  TIPOSCATEGORIAS: "http://localhost:5000/tipocategoria",
+  TIPOSCATEGORIAS_ADD: "http://localhost:5000/tipocategoria/add",
+  // OFERTAS RUTAS
+  OFERTAS: "http://localhost:5000/ofertas",
+  OFERTAS_ADD: "http://localhost:5000/ofertas/add",
+  // TIPOSUSARIOS RUTAS
+  TIPOSUSARIOS: "http://localhost:5000/tipousuario",
+  TIPOSUSARIOS_ADD: "http://localhost:5000/tipousuario/add",
+  // COMPRAS RUTAS
+  COMPRAS: "http://localhost:5000/compras/usuarios",
+  COMPRAS_ADD: "http://localhost:5000/compras/add",
+  COMPRAS_USUARIO: "http://localhost:5000/compras/usuario",
+  COMPRAS_STATUS: "http://localhost:5000/compras/estatus",
+
+  // USUARIOS RUTAS
+  USUARIOS: "http://localhost:5000/usuario",
+  USUARIOS_ADD: "http://localhost:5000/usuario/add",
+  STRIPE: "http://localhost:5000/stripe",
+};
+
+export default ENDPOINTS;
